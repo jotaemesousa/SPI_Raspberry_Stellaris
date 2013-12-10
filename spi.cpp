e0a6f21dfdeca6a36e94438944d957e9b2abb473
Joao Sousa <sousa1989joao@gmail.com>
2013-12-10 23:42:03 +0000

    a funcionar, versao 8bits, RXTO interrupt

--- spi.cpp
+++ spi.cpp
@@ -10,89 +10,96 @@ using namespace std;
  
 #define ARRAY_SIZE(array) sizeof(array)/sizeof(array[0])
  
-struct ROSCASDataToSTELLARIS
+typedef struct __attribute__((__packed__)ROSCASDataToSTELLARIS_
 {
     int8_t v_linear;
     int8_t v_angular;
-    uint8_t cmd;
-};
+    
+    uint8_t cmd;		// 0b <blinky_bit> <ask_data_bit> <HBridgeDriveMode> <ask_firmware_bit> <0> <0> <0> <start_stop_car_bit>
+}ROSCASDataToSTELLARIS;
 
-struct ROSCASDataFromSTELLARIS
+
+typedef struct __attribute__((__packed__))ROSCASDataFromSTELLARIS_
 {
-    uint8_t var1;
-    uint8_t var2;
-};
+    int32_t left_encoder_count;
+    int32_t right_encoder_count;
+    uint8_t battery_voltage;
+    uint8_t battery_current;
 
+    uint8_t cmd_back;
+    
+}ROSCASDataFromSTELLARIS;
  
 int main(int argc, char **argv) 
 {
 	int i,fd;
 	char wr_buf[]={0xf1,0xf2,0xf3,0xf4};
-	char rd_buf[10];;
- 
-	if (argc<2) {
-		printf("Usage:\n%s [device]\n", argv[0]);
-		exit(1);
-	}
-   	
-	fd = open(argv[1], O_RDWR);
-	if (fd<=0) { 
-		printf("%s: Device %s not found\n", argv[0], argv[1]);
-		exit(1);
-	}
+	char rd_buf[10];
 	
+	fd = open("/dev/spidev0.0", O_RDWR);
+		
 	int cmd = 0, l = -1, a = -128;
-	if(argc == 5)
+	if(argc == 4)
 	{
-	    sscanf(argv[2], "%d", &l);
-	    sscanf(argv[3], "%d", &a);
-	    sscanf(argv[4], "%d", &cmd);
+	    sscanf(argv[1], "%d", &l);
+	    sscanf(argv[2], "%d", &a);
+	    sscanf(argv[3], "%d", &cmd);
 	}
-	int t[]={0x20,0x21,0x33,0x45};
+	uint8_t t[]={0x20,0x21,0x22,0x23};
 	
-	
+//          write(fd,t,1);
+//          write(fd,t+1,1);
+//         write(fd,t+2,1);
+//         write(fd,t+3,1);
+// // 	write(fd,t+4,1);
+// 	
+// 	usleep(200);
+// 	uint8_t p_rec[20];
+// 	
+// 	for(int i = 0; i < 12; i++)
+// 	{
+// 	    read(fd, p_rec + i, 1);
+// 	    printf("byte = %x\n" , p_rec[i]);
+// 	}
 
-    struct ROSCASDataToSTELLARIS cmd_vel;
+    ROSCASDataToSTELLARIS cmd_vel;
     cmd_vel.v_linear = l;
     cmd_vel.v_angular = a;
     cmd_vel.cmd = cmd;
     uint8_t *p_valor = (uint8_t *)&cmd_vel;
     
-    cout << sizeof(struct ROSCASDataToSTELLARIS) << endl;
+    cout << sizeof(ROSCASDataToSTELLARIS) << endl;
 
-    for(int i = 0; i < sizeof(struct ROSCASDataToSTELLARIS); i++)
+    for(int i = 0; i < sizeof(ROSCASDataToSTELLARIS); i++)
     {
         write(fd,p_valor + i,1);
     }
 
     //usleep(100);
     
-    struct ROSCASDataFromSTELLARIS received;
-    received.var1 = 0;
-    received.var2 = 0;
+    ROSCASDataFromSTELLARIS received;
+    received.left_encoder_count = 0;
+    received.right_encoder_count = 0;
+    received.battery_voltage = 0;
+    received.battery_current = 0;
+    received.cmd_back = 0;
+    
     uint8_t *p_rec = (uint8_t *)&received;
     
-    cout << sizeof(struct ROSCASDataFromSTELLARIS) << endl;
+    cout << sizeof(ROSCASDataFromSTELLARIS) << endl;
     
     if(cmd_vel.cmd == 1)
     {
       cout << "A receber .. " << endl;
-      for(int i = 0; i < (int)sizeof(struct ROSCASDataFromSTELLARIS); i++)
+      for(int i = 0; i < (int)sizeof(ROSCASDataFromSTELLARIS); i++)
       { 
 	  read(fd, p_rec + i, 1);
-	  cout << "cenas\n" << endl;
-	  printf("byte = %x\n" , p_rec[i]);
-	
-	  
-	  cout << i  << endl;
       }
-      
-      
-      
-      
-      cout << "recebeu var1 = " << (int)received.var1 << " e var2 = " << (int)received.var2 << endl;
+      cout << "left_encoder_count = " << (int)received.left_encoder_count << "\n right_encoder_count = " << (int)received.right_encoder_count << endl;
+      cout << "battery_voltage = " << (int)received.battery_voltage << "\n battery_current = " << (int)received.battery_current << endl;
+      cout << "cmd back = " << (int)received.cmd_back << endl;
     }
-	
+
 	/*
 	if (write(fd, wr_buf, ARRAY_SIZE(wr_buf)) != ARRAY_SIZE(wr_buf))
 		perror("Write Error");
@@ -104,7 +111,7 @@ int main(int argc, char **argv)
 		if (i%2)
 			printf("\n");
 	}*/
- 
+
 	close(fd);
 	return 0;
 }
